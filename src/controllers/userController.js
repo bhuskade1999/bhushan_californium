7191eefc86ae65bcb6bc375ec6ef77bbd68274a1
bhushan <bbhuskade123@gmail.com>
2022-12-28 23:30:27 +0530

    Assighment Submiteed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,23 +2,42 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 /* ======================================================================== */
+
 const createUsers = async function (req,res){
+  try{
   let data = req.body
   let savedata = await userModel.create(data)
-  res.send({msg: savedata})
+  res.status(201).send({msg: savedata})
+
+  }
+  catch(error){
+    res.send({msg:error.message})
+  }
+
+
+
+
 }
 
 /* ======================================================================== */
 const loginUser = async function(req,res){
+  try{
   let userId = req.body.userId
   let password1 = req.body.password1
   let  checkuser = await userModel.findOne({emailId: userId, password:password1})
   
    if(!checkuser){
-       return res.send({status :false , msg : "User Not Found"})
+       return res.status(404).send({status :false , msg : "User Not Found"})
    }
       let token = jwt.sign({userId: checkuser._id.toString(),},"functionup" );
       res.send({ status: true, token: token });
+
+  }catch(error){
+    res.send({msg:error.message})
+  }
+
+
+
 };
 
 /* ======================================================================================================== */
@@ -41,16 +60,29 @@ const updateUser = async function (req,res){
 userId =req.params.userId
 data =req.body
 let savedata = await userModel.findOneAndUpdate({_id:userId},data,{new:true})
-res.send({msg : savedata})
+ 
 
 }
 
 /* ====================================================================================================== */
 
 const deleteUser = async function(req,res){
+  try{
   userId = req.params.userId
   let savedata = await userModel.findOneAndUpdate({_id:userId},{$set:{isDeleted :true}},{new:true})
   res.send({msg :savedata})
+  }
+catch(error){
+
+
+
+}
+
+
+
+
+
+
 }
 
 
